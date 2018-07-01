b3f1125b333d2d175669db1dbd0c391ee977677e
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2018-07-01 20:16:43 +0900

    create ec2instance

--- init.tf
+++ init.tf
@@ -1,10 +1,17 @@
-provider "aws" {
-	region = "ap-northeast-1a"
-}
+variable "pub_key" {}
 
+provider "aws" {
+	region = "ap-northeast-1"
+}
 
 resource "aws_instance" "backend" {
   ami = "ami-bec974d8"
 	instance_type = "t2.micro"
+	key_name = "${aws_key_pair.backend.key_name}"
+}
+
+resource "aws_key_pair" "backend" {
+  key_name   = "default_ssh_key"
+  public_key = "${var.pub_key}"
 }
 
