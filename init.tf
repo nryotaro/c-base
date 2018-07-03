e454f78b0008bad004931b4ae692ed95f164937e
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2018-07-04 01:23:02 +0900

    add security group

--- init.tf
+++ init.tf
@@ -1,4 +1,5 @@
 variable "pub_key" {}
+variable "vpc_id" {}
 
 provider "aws" {
 	region = "ap-northeast-1"
@@ -8,6 +9,7 @@ resource "aws_instance" "backend" {
   ami = "ami-bec974d8"
 	instance_type = "t2.micro"
 	key_name = "${aws_key_pair.backend.key_name}"
+	security_groups = ["${aws_security_group.ctt.id}"]
 }
 
 resource "aws_key_pair" "backend" {
@@ -15,3 +17,13 @@ resource "aws_key_pair" "backend" {
   public_key = "${var.pub_key}"
 }
 
+resource "aws_security_group" "c-base" {
+   name = "ctt"
+	 vpc_id = "${var.vpc_id}"
+   ingress {
+     from_port = 22
+		 to_port = 22
+		 protocol = "tcp"
+		 cider_blocks = ["0.0.0.0/0"]
+	 }
+}
